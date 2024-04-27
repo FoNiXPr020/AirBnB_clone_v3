102000b34667e0f812c44f889998d08cf2e06b0e
Anashm24 <anashoumaid19@gmail.com>
2024-04-27 23:46:51 +0100

    new

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3xx
+'''api status'''
+import models
+from models import storage
+from models.base_model import BaseModel
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    '''return stuff'''
+    return jsonify(status='OK')
+
