772c821b5317623baacf18cabb689aa57f9ef47c
Anashm24 <anashoumaid19@gmail.com>
2024-04-28 01:38:59 +0100

    new

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,27 @@ from models import storage
 from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 @app_views.route('/status', strict_slashes=False)
-def status():
+def returnstuff():
     '''return stuff'''
-    return jsonify(status='OK')
+    data = {"status": "OK"}
+    return jsonify(data)
 
+
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
+    '''JSON Responses'''
+    types = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in types:
+        types[key] = storage.count(types[key])
+    return jsonify(types)
