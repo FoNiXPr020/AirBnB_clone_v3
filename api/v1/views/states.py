072b76816234f178ae1849aa0881ab48959c3f4d
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-04-28 19:19:20 +0100

    Updating States.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,19 +1,50 @@
 #!/usr/bin/python3
-''' new view for State objects'''
-from flask import Flask, jsonify, request, abort
+"""State objects that handles all default RESTFul API actions"""
+
 from api.v1.views import app_views
-from os import name
-from models.state import State
-import models
 from models import storage
+from models.state import State
+from flask import abort, request, jsonify
 
 
-@app_views.route("/states", method=['GET'], strict_slashes=False)
-def get_method():
-    """retrieve all the data related to state object"""
-    objects = storage.all("State")
-    list_states = []
-    for state in objects.values():
-        list_states.append(state.to_dict())
-    return jsonify(list_states)
+@app_views.route("/states", strict_slashes=False, methods=["GET", "POST", "DELETE"])
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET", "PUT", "DELETE"])
+def states(state_id=None):
+    """show states and states with id"""
+    states_list = []
+    if state_id is None:
+        all_objs = storage.all(State).values()
+        for v in all_objs:
+            states_list.append(v.to_dict())
+        return jsonify(states_list)
+
+    obj = storage.get(State, state_id)
+    if obj is None:
+        abort(404)
+
+    if request.method == "GET":
+        return jsonify(obj.to_dict())
+
+    if request.method == "DELETE":
+        storage.delete(obj)
+        storage.save()
+        return jsonify({}), 200
+
+    if request.method == "PUT":
+        data = request.get_json(force=True, silent=True)
+        if not data:
+            abort(400, "Not a JSON")
+        obj.name = data.get("name", obj.name)
+        obj.save()
+        return jsonify(obj.to_dict()), 200
+
+    if request.method == "POST":
+        data = request.get_json(force=True, silent=True)
+        if not data:
+            abort(400, "Not a JSON")
+        if "name" not in data:
+            abort(400, "Missing name")
+        new_state = State(**data)
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
 
