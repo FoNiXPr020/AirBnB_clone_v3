1c16df654efb869b02d6e9b578ba63676aae8b20
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-04-28 21:54:11 +0100

    Updating Base_model and user for new MD5 Password

--- models/user.py
+++ models/user.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
-import hashlib
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -17,8 +18,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="all,delete", backref="user")
-        reviews = relationship("Review", cascade="all,delete", backref="user")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
