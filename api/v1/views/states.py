fcbdb50fe42234081a8907895e7389adcd3d925d
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-04-28 19:33:01 +0100

    Updating States.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,44 +7,54 @@ from models.state import State
 from flask import abort, request, jsonify
 
 
-@app_views.route("/states", strict_slashes=False, methods=["GET", "POST"])
-@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET", "PUT", "DELETE"])
+@app_views.route("/states", strict_slashes=False, methods=["GET"])
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"])
 def states(state_id=None):
-    """show states and states with id"""
+    """Show states and states with id"""
+    states_list = []
     if state_id is None:
         all_objs = storage.all(State).values()
-        return jsonify([v.to_dict() for v in all_objs])
+        for v in all_objs:
+            states_list.append(v.to_dict())
+        return jsonify(states_list)
+    else:
+        result = storage.get(State, state_id)
+        if result is None:
+            abort(404)
+        return jsonify(result.to_dict())
 
+
+@app_views.route("/states", strict_slashes=False, methods=["POST"])
+def create_state():
+    """create a new post req"""
+    data = request.get_json(force=True, silent=True)
+    if not data:
+        abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route("/states/<state_id>", strict_slashes=False,
+                 methods=["PUT", "DELETE"])
+def states_methods(state_id):
+    """Handles DELETE, PUT default RESTFul API actions"""
     obj = storage.get(State, state_id)
-    if obj is None:
-        abort(404)
-
-    if request.method == "GET":
-        return jsonify(obj.to_dict())
-
-    if request.method == "DELETE":
-        storage.delete(obj)
-        storage.save()
-        return jsonify({}), 200
 
     if request.method == "PUT":
+        if obj is None:
+            abort(404)
         data = request.get_json(force=True, silent=True)
         if not data:
             abort(400, "Not a JSON")
-        if "name" not in data:
-            abort(400, "Missing name")
-        for k, v in data.items():
-            if k not in ["id", "created_at", "updated_at"]:
-                setattr(obj, k, v)
+        obj.name = data.get("name", obj.name)
         obj.save()
         return jsonify(obj.to_dict()), 200
 
-    if request.method == "POST":
-        data = request.get_json(force=True, silent=True)
-        if not data:
-            abort(400, "Not a JSON")
-        if "name" not in data:
-            abort(400, "Missing name")
-        new_state = State(**data)
-        new_state.save()
-        return jsonify(new_state.to_dict()), 201
+    if request.method == "DELETE":
+        if obj is None:
+            abort(404)
+        storage.delete(obj)
+        storage.save()
+        return jsonify({}), 200
