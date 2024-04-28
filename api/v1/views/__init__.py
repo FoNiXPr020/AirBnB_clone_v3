d5ca02eac7f4827c4abdf66bcfe1f643b55bef6c
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-04-28 19:40:09 +0100

    Update init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,9 @@
 """sharing app_views Blueprint"""
 
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
+
+from .index import *
+from .states import *
 
 
 app_views = Blueprint('app_views', __name__)
